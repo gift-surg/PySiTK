b98e9b516be6054a7c84c8f90025c342f2ddbc6c
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2017-10-02 11:57:35 +0100

    STYLE: Update of definitions.py to avoid multiple definitions; Use of $DIRTMP
    DIRTMP as variable so that it can also be used on cluster (I hope that is
    generically used on most of the systems ...)

--- pythonhelper/definitions.py
+++ pythonhelper/definitions.py
@@ -3,14 +3,12 @@ import sys
 
 DIR_ROOT = os.path.dirname(os.path.abspath(__file__))
 DIR_TEST = os.path.join(DIR_ROOT, "..", "data")
-DIR_TMP = "/tmp/"
+DIR_TMP = os.environ['TMPDIR']
+# DIR_TMP = "/tmp/"
 
 # Linked executables
 ITKSNAP_EXE = "itksnap"
+FSLVIEW_EXE = "fsleyes"  # Viewer in FSL 5.0.10
 # FSLVIEW_EXE = "fslview" # deprecated in FSL 5.0.10 -> fslview_deprecated
 # FSLVIEW_EXE = "fslview_deprecated"
-FSLVIEW_EXE = "fsleyes"  # Viewer in FSL 5.0.10
 NIFTYVIEW_EXE = "NiftyView"
-BET_EXE = "bet"
-REG_ALADIN_EXE = "reg_aladin"
-REG_F3D_EXE = "reg_f3d"
